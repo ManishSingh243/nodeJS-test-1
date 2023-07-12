efd41badc3d136080c9ff1c950434468eee1f8d4
singhmanish975310@gmail.com <singhmanish975310@gmail.com>
2023-07-12 10:13:17 +0530

    fifth commit

---
+++ routes/success.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+const express = require('express');
+
+const productController = require('../controllers/product')
+
+const router = express.Router();
+
+router.get('/success', productController.getSuccess)
+
+module.exports = router;
