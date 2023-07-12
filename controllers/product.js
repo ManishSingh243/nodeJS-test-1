d5a859ef5eaffa10ad39b6096438981678be4674
singhmanish975310@gmail.com <singhmanish975310@gmail.com>
2023-07-12 10:06:29 +0530

    second commit

---
+++ controllers/product.js
@@ -0,0 +1,36 @@
+const products = [];
+
+exports.getAddProduct = (req, res, next) => {
+    res.render('add-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/add-product',
+      formsCSS: true,
+      productCSS: true,
+      activeAddProduct: true
+    });
+  }
+
+  exports.postAddProduct = (req, res, next) => {
+    products.push({ title: req.body.title });
+    res.redirect('/');
+  }
+
+  exports.getProduct = (req, res, next) => {
+    //const products = adminData.products;
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
+  }
+
+  exports.getContact = (req, res)=>{
+    res.send("<html><body><form action='/admin/add-product' method='POST'><h1>Welcome to contact page!!!</h1><button type='submit'>Home</button></form></body></html>")
+}
+
+exports.getSuccess = (req, res)=>{
+  res.send("<html><body><form action='/admin/add-product' method='POST'><h1>Success!!!</h1><button type='submit'>Home</button></form></body></html>")
+}
